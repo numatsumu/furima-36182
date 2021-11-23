81bd1a01ff73e6f7c49705720d32777f89f73b24
numatsumu <auchinumax@gmail.com>
2021-11-23 10:06:58 +0900

    rubocopの指摘事項修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,12 +11,12 @@ class Item < ApplicationRecord
 
   validates :product_name,       presence: true
   validates :description,        presence: true
-  validates :category_id,        presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :condition_id,       presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipping_charge_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :prefecture_id,      presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :days_to_ship_id,    presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price,              presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/ }
+  validates :category_id,        presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id,       presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_charge_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id,      presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :days_to_ship_id,    presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price,              presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300,
+                                                                 less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
   validates :image,              presence: true
-
 end
