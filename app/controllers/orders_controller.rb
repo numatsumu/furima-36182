9811b6df2ddcb3b6812af7173fb0d6db43e56c83
numatsumu <auchinumax@gmail.com>
2021-12-05 15:15:10 +0900

    商品購入機能の実装

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,45 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :create]
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: :index
+
+  def index
+    @order_destination = OrderDestination.new
+  end
+
+  def create
+    @order_destination = OrderDestination.new(order_params)
+    if @order_destination.valid?
+      pay_item
+      @order_destination.save
+      redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_destination).permit(:postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number, :number, :cvc, :exp_month, :exp_year)
+    .merge(item_id: @item.id).merge(user_id: current_user.id).merge(token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def move_to_index
+    if @item.order.present? || current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+end
